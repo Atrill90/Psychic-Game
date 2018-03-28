cea3529008129b74e5ff680d81ba8a72e916f10d
Alex Trilling <atrill90@gmail.com>
2018-03-28 00:58:50 -0400

    changed computerGuess to = function of generateLetter, fixed scoreboard issue, and made computer pick new letter after win and loss

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,60 +1,50 @@
-var alphabet = [ "a", "b", "c", "d", "e", "f,", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r","s","t","u","v","w","x","y","z"];
+var alphabet = ["a", "b", "c", "d", "e", "f,", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var wins = 0;
 var losses = 0;
 let guesses = 10;
 
 var container = document.querySelector(".container");
 
+function generateLetter() {
+    console.log("newLetterGenerated");
+   return alphabet[Math.floor(Math.random() * alphabet.length)];
+    
+}
 
 
-
-var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+var computerGuess = generateLetter();
 console.log(computerGuess);
 
-document.querySelector(".userInput").addEventListener("keypress", function(e){
- var   html = 
+document.querySelector(".userInput").addEventListener("keypress", function (e) {
+
+
+    var userGuess = e.key;
+
+    if (guesses > 0) {
+        if (computerGuess === userGuess) {
+            wins++;
+           computerGuess = generateLetter();
+           console.log(computerGuess);
+            guesses = 10;
+        } else {
+            guesses--;
+        }
+        document.querySelector(".userInput").value = "";
+
+    } else {
+        losses++;
+        computerGuess = generateLetter();
+        console.log(computerGuess);
+        guesses = 10;
+
+    }
+    var html =
     `<p> guesses ${guesses} </p>
     <p> wins ${wins}</p>
     <p> losses ${losses}</p>`;
 
-    var userGuess = e.key;
-    console.log(wins);
-    if (guesses > 0 ) {
-        if (computerGuess === userGuess) {
-            wins++;
-            console.log(wins);
-            guesses = 10;
-            document.querySelector(".userInput").value = "";
-            randomKey
-            container.innerHTML = html
-        }else{
-            guesses--;
-            document.querySelector(".userInput").value = "";
-            container.innerHTML = html
-        }
-    }else{
-        losses++;
-        guesses = 10;
-        container.innerHTML = html
-    
-    }
-     function randomKey(userGuess)
-     
+    container.innerHTML = html
+
+
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
